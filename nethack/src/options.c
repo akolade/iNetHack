68d5cfbed0816abc2146d34d0c8586b188c111c1
akolade <akolade@gmail.com>
2023-03-18 16:50:21 -0600

    IBMGraphics: Update core to Nethack 3.6.7.

--- nethack/src/options.c
+++ nethack/src/options.c
@@ -2832,7 +2832,7 @@ boolean tinitial, tfrom_file;
             return FALSE;
         } else if ((op = string_for_opt(opts, negated)) != empty_optstr) {
 #if defined(WIN32) && defined(TTY_GRAPHICS)
-            set_altkeyhandler(op);
+            set_altkeyhandling(op);
 #endif
         } else
             return FALSE;
@@ -5589,7 +5589,11 @@ boolean setinitial, setfromfile;
             assign_graphics(PRIMARY);
         preference_update("symset");
         need_redraw = TRUE;
-
+#ifdef WIN32
+    } else if (!strcmp("altkeyhandler", optname)
+               || !strcmp("altkeyhandling", optname)) {
+        return set_keyhandling_via_option();
+#endif
     } else {
         /* didn't match any of the special options */
         return FALSE;
@@ -5629,7 +5633,11 @@ char *buf;
 #ifdef WIN32
     else if (!strcmp(optname, "altkeyhandler"))
         Sprintf(buf, "%s",
-                iflags.altkeyhandler[0] ? iflags.altkeyhandler : "default");
+                (iflags.key_handling == ray_keyhandling)
+                    ? "ray"
+                    : (iflags.key_handling == nh340_keyhandling)
+                        ? "340"
+                        : "default");
 #endif
 #ifdef BACKWARD_COMPAT
     else if (!strcmp(optname, "boulder"))
@@ -6929,7 +6937,7 @@ void
 set_playmode()
 {
     if (wizard) {
-        if (/* jrd authorize_wizard_mode()*/ 0)
+        if (/* authorize_wizard_mode()*/ 0)
             Strcpy(plname, "wizard");
         else
             wizard = FALSE; /* not allowed or not available */
